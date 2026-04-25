fc4464262ae1ea73f9bbe8473d69b42901c6250b
vivutrongchivi-hash <vivutrongchivi@gmail.com>
2026-04-25 21:00:25 +0700

    Update firmware file paths for FTS upgrade

--- drivers/input/touchscreen/mediatek/ft8006s_spi/focaltech_config.h
+++ drivers/input/touchscreen/mediatek/ft8006s_spi/focaltech_config.h
@@ -249,13 +249,13 @@
  * if FTS_GET_MODULE_NUM >= 2, fw corrsponding with FTS_VENDOR_ID2
  * define your own fw_file, the sample one is invalid
  */
-#define FTS_UPGRADE_FW2_FILE                     "include/firmware/fw_sample.i"
+#define FTS_UPGRADE_FW2_FILE                     "include/firmware/fw_helitai_v0e.i"
 
 /*
  * if FTS_GET_MODULE_NUM >= 3, fw corrsponding with FTS_VENDOR_ID3
  * define your own fw_file, the sample one is invalid
  */
-#define FTS_UPGRADE_FW3_FILE                     "include/firmware/fw_sample.i"
+#define FTS_UPGRADE_FW3_FILE                     "include/firmware/fw_helitai_v0e.i"
 
 /*********************************************************/
 
